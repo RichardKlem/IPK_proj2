064a6472a6d4f97d64e990615393005819422acc
Richard Klem <richard.klem@seznam.cz>
2020-04-26 01:35:23 +0200

    osetreni nullptr na vstupu

--- my_string.cpp
+++ my_string.cpp
@@ -11,9 +11,14 @@
  * @return int výstupní struktura obsahující dvojici (status, číslo)
  */
 str2int_struct_t str2int (char * str){
-    std::stringstream ss(str);
-    int num;
     str2int_struct_t result {S2I_FAIL, 0};
+    int num;
+
+    if (str == nullptr)
+        return result;
+
+    std::stringstream ss(str);
+
     if((ss >> num).fail())
         return result;
     result = {S2I_OK, num};
